274f0e03eea9d0475fdac63f21730ef1dc4fe30b
Gustavo Martin <gustavo.martin@alumnos.udg.mx>
2024-01-06 21:24:10 -0600

    Spo2 cambios

--- controllers/spo2.js
+++ controllers/spo2.js
@@ -29,7 +29,7 @@ module.exports.guadarInfo = async (req, res) => {
 
 module.exports.recibirInfo = async (req, res) => {
     const resultado = await Spo2.find({idPaciente: req.session.paciente})
-    if(resultado.length===0){
+    if(resultado.length === 0){
         res.sendStatus(200)
     }else{
         res.send(resultado)
--- public/javascripts/spo2.js
+++ public/javascripts/spo2.js
@@ -1,6 +1,5 @@
 const spo2Dato = document.getElementById('spo2Dato')
 const spo2Icon = document.getElementById('spo2Icon')
-const estadoDispositivo = document.getElementById('deviceState')
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -11,7 +10,6 @@ async function recibirInformacion() {
         try{
             const response = await fetch("http://localhost:3000/spo2/guardar");
             const dato = await response.json();
-            estadoDispositivo.innerText = "Estado: Conectado"
             if((dato[0].lectura >= 92 && dato[0].lectura<=100) || dato[0].lectura === -1){
                 spo2Dato.classList.remove("text-danger")
                 spo2Dato.classList.add("text-primary")
@@ -23,9 +21,9 @@ async function recibirInformacion() {
                 spo2Icon.classList.remove("bg-light")
                 spo2Icon.classList.add("bg-danger")
             }
+            spo2Sensor = document.getElementById('spo2Sensor')
             if(dato[0].lectura === -1){
                 spo2Dato.innerText = "Apagado"
-                spo2Sensor = document.getElementById('spo2Sensor')
                 spo2Sensor.checked = false
             }else{
                 spo2Dato.innerText = dato[0].lectura  
@@ -39,7 +37,6 @@ async function recibirInformacion() {
             spo2Dato.innerText = "Apagado"
             spo2Sensor = document.getElementById('spo2Sensor')
             spo2Sensor.checked = false
-            estadoDispositivo.innerText = "Estado: Desconectado"
         }
         
         await sleep(1000)
